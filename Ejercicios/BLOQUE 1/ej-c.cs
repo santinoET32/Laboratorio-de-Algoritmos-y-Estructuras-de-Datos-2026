e65adc7bde11451c5e3efe7b51d95e93947742d4
santinoET32 <santino.mendezet32@gmail.com>
2026-06-09 13:44:13 -0300

    Update ej-c.cs

--- Ejercicios/BLOQUE 1/ej-c.cs
+++ Ejercicios/BLOQUE 1/ej-c.cs
@@ -1,26 +1,9 @@
-class ejercicioD
+class ejercicioC
 {
     static void Main()
     {
-        string contraseña = "AJXDR";
-        int intentos = 0;
-        while (intentos < 5)
-        {
-            Console.WriteLine("Escribi la contraseña: ");
-            string intento = Console.ReadLine();
-            intentos++;
-            if (intento == contraseña)
-            {
-                Console.WriteLine("Contraseña correcta");
-            }
-            else
-            {
-                Console.WriteLine($"Contraseña incorrecta te quedan {5 - intentos} intentos");
-            }
-        }
-        if (intentos == 5)
-        {
-            Console.WriteLine("Agotaste los intentos");
-        }
+        Console.WriteLine("Escribi una palabra: ");
+        string palabra = Console.ReadLine();
+        Console.WriteLine($"La palabra tiene {palabra.Length} letras.");
     }
 }
